14e7d08457b335f29ab7d2a96c5e54cfab17fd09
memory4963 <bolo4963@gmail.com>
2017-11-26 18:35:56 +0800

    完成EnvResultActivity

--- app/src/main/java/com/squarecircle/automonkeytest/Activity/EnvResult/EnvResultActivity.java
+++ app/src/main/java/com/squarecircle/automonkeytest/Activity/EnvResult/EnvResultActivity.java
@@ -3,6 +3,8 @@ package com.squarecircle.automonkeytest.Activity.EnvResult;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -26,7 +28,9 @@ import java.util.regex.Pattern;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import lecho.lib.hellocharts.formatter.SimpleAxisValueFormatter;
 import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
 import lecho.lib.hellocharts.model.Line;
 import lecho.lib.hellocharts.model.LineChartData;
 import lecho.lib.hellocharts.model.PointValue;
@@ -34,55 +38,119 @@ import lecho.lib.hellocharts.model.ValueShape;
 import lecho.lib.hellocharts.model.Viewport;
 import lecho.lib.hellocharts.view.LineChartView;
 
-public class EnvResultActivity extends AppCompatActivity {
+public class EnvResultActivity extends AppCompatActivity implements Destroyable {
     
     protected final String TAG = this.getClass().getSimpleName();
     
     @BindView(R.id.env_result_toolbar) Toolbar toolbar;
+    
+    //chartView
     @BindView(R.id.env_result_chart) LineChartView chartView;
     List<PointValue> cpuList = new ArrayList<>();
     List<PointValue> memList = new ArrayList<>();
     List<Line> lineList = new ArrayList<>();
     int position = 0;
+    int memMax = 100;
+    private Axis axisMem = new Axis();
     
-    Axis axisx = new Axis();
-    Axis axisy = new Axis();
-    
+    //data
     int threadCount, pictureCount, CPUInterval, memoryInterval;
-    
     float totalCPU = 0, idleCPU = 0, totalCPU2, idleCPU2, processCPU = 0, processCPU2;
     int CPUnum, processMemory;
     float totalCpuRatio, processCpuRatio;
     int pid = android.os.Process.myPid();
+    
+    //加载图片
+    ArrayList<Bitmap> bmpList = new ArrayList<>();
+    
+    //开启线程
+    ArrayList<Thread> threads = new ArrayList<>();
+    boolean destroyed = false;
+    
+    //timers
+    CountDownTimer cpuTimer;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_env_result);
         ButterKnife.bind(this);
-        
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         
-        Intent intent = getIntent();
-        threadCount = intent.getIntExtra(EnvFragment.THREAD_COUNT, 20);
-        pictureCount = intent.getIntExtra(EnvFragment.PICTURE_COUNT, 100);
-        CPUInterval = intent.getIntExtra(EnvFragment.CPU_INTERVAL, 3);
-        memoryInterval = intent.getIntExtra(EnvFragment.MEMORY_INTERVAL, 3);
+        initChartView();
         
-        axisx.setName("时间");
-        axisy.setName("百分比");
+        geneData();
+
+        loadPic();
+        openThread();
         
-        Log.d(TAG, "onCreate: " + getNumCores());
+        initCpuTimer();
         
-        CountDownTimer timer = new CountDownTimer(1000000000, 500) {
+        cpuTimer.start();
+        
+    }
+    
+    private void loadPic() {
+        threads.add(new Thread(new Runnable() {
+            
+            @Override
+            public void run() {
+                for (int i = 0; i < pictureCount; i++) {
+                    bmpList.add(BitmapFactory.decodeResource(getResources(), R.drawable.test));
+                }
+            }
+        }));
+    }
+    
+    private void openThread() {
+        for (int i = 0; i < threadCount - 1; i++) {
+            threads.add(new Thread(getRunnable()));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+    }
+    
+    private Runnable getRunnable() {
+        return new MyRunnable(this);
+    }
+    
+    @Override
+    public boolean getDestroyed() {
+        return destroyed;
+    }
+    
+    private class MyRunnable implements Runnable {
+        
+        private Destroyable destroyable;
+        
+        public MyRunnable(Destroyable destroyable) {
+            this.destroyable = destroyable;
+        }
+        
+        @Override
+        public void run() {
+            while (!destroyable.getDestroyed()) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    private void initCpuTimer() {
+        cpuTimer = new CountDownTimer(1000000000, CPUInterval * 1000) {
             
             @Override
             public void onTick(long millisUntilFinished) {
                 getTotalCpuTime();
                 getProcessCpuTime();
                 getPidMemorySize(EnvResultActivity.this);
-                changeData();
+                Log.d(TAG, "onTick");
+                changeStatus();
             }
             
             @Override
@@ -90,46 +158,113 @@ public class EnvResultActivity extends AppCompatActivity {
                 Toast.makeText(EnvResultActivity.this, "完成", Toast.LENGTH_SHORT).show();
             }
         };
-        timer.start();
+    }
+    
+    private void geneData() {
+        Intent intent = getIntent();
+        threadCount = intent.getIntExtra(EnvFragment.THREAD_COUNT, 20);
+        pictureCount = intent.getIntExtra(EnvFragment.PICTURE_COUNT, 10);
+        CPUInterval = intent.getIntExtra(EnvFragment.CPU_INTERVAL, 3);
+        
+        Log.d(TAG, "onCreate: " + getNumCores());
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cpuTimer.cancel();
+        
+        for (Bitmap bmp : bmpList) {
+            bmp.recycle();
+        }
         
     }
     
-    void changeData() {
+    private void initChartView() {
         
-        PointValue cpuPoint = new PointValue(position, totalCpuRatio);
-        cpuList.add(cpuPoint);
+        Axis axisTime = new Axis();
+        Axis axisCpu = new Axis();
+        
+        axisTime.setName("时间");
+        axisCpu.setName("百分比");
+        axisMem.setName("内存占用量");
+        
+        ArrayList<AxisValue> values = new ArrayList<>();
+        for (int i = 0; i < 11; i++) {
+            AxisValue axisValue = new AxisValue(i * 10);
+            axisValue.setLabel("" + i);
+            values.add(axisValue);
+        }
+        axisCpu.setValues(values);
+        axisCpu.setFormatter(new SimpleAxisValueFormatter().setAppendedText("%".toCharArray()));
+        axisCpu.setHasLines(true);
+        
+        ArrayList<AxisValue> memValues = new ArrayList<>();
+        for (int i = 0; i < 11; i++) {
+            AxisValue axisValue = new AxisValue(i * 10);
+            axisValue.setLabel("" + i);
+            memValues.add(axisValue);
+        }
+        axisMem.setValues(memValues);
+        
+        //cpu折线蓝色
         Line cpuLine = new Line(cpuList);
         cpuLine.setColor(Color.BLUE);
         cpuLine.setShape(ValueShape.CIRCLE);
         cpuLine.setCubic(false);
-    
-        PointValue memPoint = new PointValue(position, processMemory);
-        memList.add(memPoint);
+        //memory折线红色
         Line memLine = new Line(memList);
         memLine.setColor(Color.RED);
         memLine.setShape(ValueShape.CIRCLE);
         memLine.setCubic(false);
         
-        lineList.clear();
         lineList.add(cpuLine);
-//        lineList.add(memLine);
+        lineList.add(memLine);
         
         LineChartData data = new LineChartData(lineList);
-        data.setAxisXBottom(axisx);
-        data.setAxisYLeft(axisy);
+        data.setAxisXBottom(axisTime);
+        data.setAxisYLeft(axisCpu);
+        data.setAxisYRight(axisMem);
         chartView.setLineChartData(data);
         
-        float x = cpuPoint.getX();
-        Viewport viewport;
-        if (x > 10) {
-            viewport = initViewPort(x - 10, x);
-        } else {
-            viewport = initViewPort(0, 10);
-        }
-        chartView.setCurrentViewport(viewport);
+    }
+    
+    void changeStatus() {
         
-        Viewport maxViewPort = initMaxViewPort(x);
-        chartView.setMaximumViewport(maxViewPort);
+        PointValue cpuPoint = new PointValue(position, totalCpuRatio);
+        cpuList.add(cpuPoint);
+        
+        float processMemoryM = (float) ((processMemory - 1.0) / 1024);
+        if (processMemoryM > memMax) {
+            for (PointValue p : memList) {
+                p.set(p.getX(), p.getY() * memMax / processMemoryM);
+            }
+            memMax = (int) (processMemoryM + 0.5);
+        }
+        
+        //重新设置axisMem的label
+        List<AxisValue> values = axisMem.getValues();
+        for (int i = 0; i < 11; i++) {
+            AxisValue axisValue = values.get(i);
+            axisValue.setLabel("" + memMax / 10 * i);
+        }
+        
+        float normMem = (processMemoryM / memMax) * 100;
+        
+        PointValue memPoint = new PointValue(position, normMem);
+        memList.add(memPoint);
+        
+        chartView.setLineChartData(chartView.getLineChartData());
+        Viewport viewport = chartView.getCurrentViewport();
+    
+        if (viewport.right > 15) {
+            viewport.right += 1;
+            viewport.left = viewport.right - 15;
+        }
+        viewport.top = 100;
+        viewport.bottom = 0;
+        chartView.setCurrentViewport(viewport);
+        chartView.setMaximumViewport(viewport);
         
         position++;
     }
@@ -152,7 +287,7 @@ public class EnvResultActivity extends AppCompatActivity {
         
         //获取总体CPU占用率，其中CPUnum为CPU核心数
         totalCpuRatio =
-                (int) (100 * ((double) ((totalCPU - idleCPU) - (totalCPU2 - idleCPU2)) / (double) (
+                (float) (100 * ((double) ((totalCPU - idleCPU) - (totalCPU2 - idleCPU2)) / (double) (
                         totalCPU - totalCPU2)));
     }
     
@@ -208,24 +343,6 @@ public class EnvResultActivity extends AppCompatActivity {
         processMemory = memoryInfo[0].getTotalPss();
     }
     
-    private Viewport initViewPort(float left, float right) {
-        Viewport port = new Viewport();
-        port.top = 100;
-        port.bottom = 0;
-        port.left = left;
-        port.right = right;
-        return port;
-    }
-    
-    private Viewport initMaxViewPort(float right) {
-        Viewport port = new Viewport();
-        port.top = 100;
-        port.bottom = 0;
-        port.left = 0;
-        port.right = right + 10;
-        return port;
-    }
-    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -234,6 +351,27 @@ public class EnvResultActivity extends AppCompatActivity {
                 return true;
             default:
                 return false;
+        }
+    }
+    
+    private static class HeightValueFormatter extends SimpleAxisValueFormatter {
+        
+        private float scale;
+        private float sub;
+        private int decimalDigits;
+        
+        public HeightValueFormatter(float scale, float sub, int decimalDigits) {
+            this.scale = scale;
+            this.sub = sub;
+            this.decimalDigits = decimalDigits;
+        }
+        
+        @Override
+        public int formatValueForAutoGeneratedAxis(char[] formattedValue, float value,
+                int autoDecimalDigits) {
+            float scaledValue = (value + sub) / scale;
+            return super.formatValueForAutoGeneratedAxis(formattedValue, scaledValue,
+                    this.decimalDigits);
         }
     }
     
